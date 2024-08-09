3ed3ad9aea6525ef9bc87927a6852e4a8b1f2ecb
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-08-09 10:12:42 +0300

    usevoucher removal

--- client/src/features/vouchers/useVouchers.ts
+++
@@ -1,9 +0,0 @@
-import vouchersAPI from "../../apis/couponsAPI";
-
-const fetchVoucherByCode = async (voucherCode) => {
-  const { useGetVoucherByCodeQuery } = vouchersAPI;
-  const voucher = await useGetVoucherByCodeQuery(voucherCode).unwrap();
-  return voucher;
-};
-
-export default fetchVoucherByCode;

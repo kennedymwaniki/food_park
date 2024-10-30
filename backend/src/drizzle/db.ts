dc7be30cfa86d90993a6986eeb4b290bd61214f0
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-10-30 19:11:56 +0300

    changes

--- backend/src/drizzle/db.ts
+++ backend/src/drizzle/db.ts
@@ -1,10 +1,37 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema"
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import * as schema from "./schema"
 
-import {config} from "dotenv"
+// import {config} from "dotenv"
 
-config({path: ".env"})
+// config({path: ".env"})
 
-const client = neon(process.env.DATABASE_URL!)
-export const db = drizzle(client, {schema, logger: true})
+// const client = neon(process.env.DATABASE_URL!)
+// export const db = drizzle(client, {schema, logger: true})
+
+//working with local postgres
+//! when using a local db
+import { config } from "dotenv";
+config({ path: ".env" });
+
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+import * as schema from "./schema";
+
+console.log("DATABASE_URL IN DRIZZLE DB.TS:", process.env.DATABASE_URL!);
+
+export const client = new Client({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const main = async () => {
+  await client.connect();
+};
+main();
+
+const db = drizzle(client, { schema, logger: true });
+
+/*
+!package json migrate for neon.
+ //! "migrate": "tsx src/drizzle/migrate.ts", migrartion script for when working with a local postgress db,
+*/
